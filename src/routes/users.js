b19bf78d9a5ef6a8e2c924c4f2fd51314b9211f8
Gonzalo Balay Losada <gbalay@denodo.com>
2026-06-11 11:24:16 +0200

    fix: add qualifiers endpoint for viewing another user's bracket

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { requireAuth } from '../middleware/auth.js'
-import { getUserTodayPredictions, getUserPredictions, getUserBracket } from '../controllers/users.controller.js'
+import { getUserTodayPredictions, getUserPredictions, getUserBracket, getUserQualifiersHandler } from '../controllers/users.controller.js'
 
 const router = Router()
 router.use(requireAuth)
@@ -8,5 +8,6 @@ router.use(requireAuth)
 router.get('/:userId/predictions/today', getUserTodayPredictions)
 router.get('/:userId/predictions', getUserPredictions)
 router.get('/:userId/bracket', getUserBracket)
+router.get('/:userId/qualifiers', getUserQualifiersHandler)
 
 export default router
