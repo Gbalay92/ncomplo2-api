a4b59f48595293687fa42422874ee17e8e597167
Gonzalo Balay Losada <gbalay@denodo.com>
2026-06-12 09:31:32 +0200

    feat: add GET /:userId/profile endpoint

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { requireAuth } from '../middleware/auth.js'
-import { getUserTodayPredictions, getUserPredictions, getUserBracket, getUserQualifiersHandler } from '../controllers/users.controller.js'
+import { getUserTodayPredictions, getUserPredictions, getUserBracket, getUserQualifiersHandler, getUserProfile } from '../controllers/users.controller.js'
 
 const router = Router()
 router.use(requireAuth)
 
+router.get('/:userId/profile', getUserProfile)
 router.get('/:userId/predictions/today', getUserTodayPredictions)
 router.get('/:userId/predictions', getUserPredictions)
 router.get('/:userId/bracket', getUserBracket)
